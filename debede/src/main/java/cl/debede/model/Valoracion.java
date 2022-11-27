4eaeeeb6ed02e3ba0e161bff724bd8d3142461ef
Kappappita <ignacioladal@gmail.com>
2022-11-27 03:43:36 -0300

    Se agregan objeto y repositorio de Valoracion

---
+++ debede/src/main/java/cl/debede/model/Valoracion.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cl.debede.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table (name = "valoracion")
+public class Valoracion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    int puntuacion;
+    String comentario;
+    boolean favorito;
+    
+    @ManyToOne
+    @JoinColumn(name = "id_user")
+    Usuario usuario;
+    
+    @ManyToOne
+    @JoinColumn(name = "id_empresa")
+    Empresa empresa;
+    
+    public Valoracion() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getPuntuacion() {
+        return puntuacion;
+    }
+
+    public void setPuntuacion(int puntuacion) {
+        this.puntuacion = puntuacion;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public boolean isFavorito() {
+        return favorito;
+    }
+
+    public void setFavorito(boolean favorito) {
+        this.favorito = favorito;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Empresa getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(Empresa empresa) {
+        this.empresa = empresa;
+    }
+    
+    
+    
+    
+}
---
+++ debede/src/main/java/cl/debede/repository/ValoracionRepository.java
@@ -0,0 +1,10 @@
+package cl.debede.repository;
+
+import cl.debede.model.Valoracion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ValoracionRepository extends JpaRepository<Valoracion, Long>{
+    
+}
